685e1a4c259e72d0762749778fd47c33cb16ceeb
Miguel04AFR <miguelflorin4@gmail.com>
2025-10-25 20:10:30 -0400

    primer push

---
+++ src/app/pagina-principal/page.tsx
@@ -0,0 +1,10 @@
+import MenuBar from '../../components/MenuBar'
+
+export default function Home() {
+  return (
+    <div>
+      <MenuBar />
+
+    </div>
+  )
+}
---
+++ src/components/MenuBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const menuBar = () => {
+  return (
+    <nav>menuBar
+
+
+
+        
+    </nav>
+  )
+}
